7ea1177004a9295b368e52c0d51d98c09cb8e3b9
Justin Ely <ely@stsci.edu>
2014-10-02 01:41:36 -0400

    added comments and README

--- UMD/AST610/HW2/hw2.py
+++ UMD/AST610/HW2/hw2.py
@@ -1,5 +1,9 @@
 """Solve the saha equation for the temperature at a given ionization and density
 
+## Some thoughts
+Why doesn't this work for large densities?  rho=100 and ionization=.75
+fails becuase the temperature is negative in the exponent
+
 """
 
 __author__ = 'Justin Ely'
@@ -10,8 +14,29 @@ import matplotlib.pyplot as plt
 #-------------------------------------------------------------------------------
 
 def saha(ionization=.5, rho=1, T=1, precision=.0001):
-    """Why doesn't this work for large densities?  rho=100 and ionization=.75
-    fails becuase the temperature is negative
+    """Solve the saha equation for temperature
+
+    The equation will be solved by recursively calling the saha function, 
+    using the newly calculated new_t value as the input to the T**(3/2.) term.
+    When the next iteration differs from less than the desired precision from
+    the previous value, the recursion will stop and the value will be returned.
+
+    Parameters
+    ----------
+    ionization : int, float
+        ionization fraction of the gas
+    rho : float, int
+        density of the gas
+    T : int, float
+        initial guess for temperature
+    precision : float, int
+        desired level of precision of calculated value
+
+    Returns
+    -------
+    new_t : float
+        the calculated temperature from iteration
+    
     """
 
     alpha = (ionization**2) / (1-ionization)
@@ -27,30 +52,31 @@ def saha(ionization=.5, rho=1, T=1, precision=.0001):
 #-------------------------------------------------------------------------------
 
 def problem_4():
- 
+    """Prepare the output for problem 4 of HW 2
+    
+    
+    """
+
     #-- 1000 evenly spaced densities from 1e-31 to 1e-16
     densities = np.linspace(1e-31, 1e-16, 1000)
     temperatures = [saha(.5, density) for density in densities]
-
 
     #-- Plotting begins here
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
 
     ax.plot(temperatures, densities)
-
-    ax.set_yscale('log')
-    ax.grid()
     
     ax.set_xlabel('Temperature $K$')
     ax.set_ylabel('$log_{10} \rho$')
-    
-    ax.set_title('Saha equation solutions')
-    
-    fig.savefig('hw4.pdf')
+    ax.set_title('Saha equation solutions for T')
+    ax.set_yscale('log')
+    ax.grid()    
+
+    fig.savefig('ely_hw2.pdf')
 
     #-- Write out the values to a file
-    with open('hw4_values.txt', 'w') as out_txt:
+    with open('ely_hw2_table.txt', 'w') as out_txt:
         out_txt.write('Log10 Density    Temperature\n')
         out_txt.write('----------------------------\n')
         for d, t in zip(densities, temperatures):
@@ -59,7 +85,7 @@ def problem_4():
 #-------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    #-- Test case from the lectures.  Should be aronud 3300 K
-    print saha(ionization=.5, rho=1.7e-24)
+    #-- Test case from the lectures.  Should be around 3300 K
+    #print saha(ionization=.5, rho=1.7e-24)
 
     problem_4()
