a55838653b993c60922c1c5ad531ce3e6831b28b
Justin Ely <justincely@gmail.com>
2015-10-03 17:02:10 -0400

    Saving before a branch to test things

---
+++ JHU/615.202.81.FA15_data_structures/module_05/PostfixEval.java
@@ -0,0 +1,70 @@
+public class PostfixEval{
+
+  public static void main(String[]args){
+    for (int i = 0; i < args.length; i++) {
+      System.out.println(args[i]);
+    }
+
+    String testing_string = "ABC*+DE-/";
+
+    translate(testing_string);
+  }
+
+
+  /**Not implemented yet
+    */
+  //public static String parseFromFile(String filename) {
+  //
+  //}
+
+  private static boolean isOperator(String character) {
+		return (character.equals("+") || character.equals("-") || character.equals("*") || character.equals("/"));
+	}
+
+  private static String selectCMD(String operator) {
+
+    if (operator.equals("+")) {
+      return "AD";
+    } else if (operator.equals("-")) {
+      return "SB";
+    } else if (operator.equals("*")) {
+      return "ML";
+    } else if (operator.equals("/")) {
+      return "DV";
+    } else {
+      return "BadOperator";
+    }
+
+  }
+
+  public static void translate(String expression) {
+    System.out.println("Input Expression: " + expression);
+
+    Stack variables = new Stack();
+    int tempNum = 1;
+    char arg1;
+    char arg2;
+    char op;
+    String command;
+
+    for (int i=0; i<expression.length(); i++){
+      if (isOperator(expression.substring(i, i+1))){
+        //Check for empty stack
+
+        command = selectCMD(expression.substring(i, i+1));
+        arg1 = variables.pop();
+        arg2 = variables.pop();
+        System.out.println(command + " " + arg1 + " " + arg2);
+        variables.push(tempNum);
+        tempNum++;
+      } else {
+        variables.push(expression.charAt(i));
+      }
+
+
+    }
+
+  }
+
+
+}
