1fcc7e178960b4a92e9c3b0a4821882ce8083364
Justin Ely <justincely@gmail.com>
2015-10-19 22:57:10 -0400

    Working I/O and correct for small numbers

--- JHU/605.204.81.FA15_computer_organization/module_07/powerball.asm
+++ JHU/605.204.81.FA15_computer_organization/module_07/powerball.asm
@@ -11,25 +11,71 @@
         la $a0, in_n
         syscall
 
-        
-
+        li $v0, 5  #read integer
+        syscall
+        addi $s0, $v0, 0
 
         li $v0, 4  # print string
         la $a0, in_k
         syscall
 
+        li $v0, 5 #read integer
+        syscall
+        addi $s1, $v0, 0
 
-        addi $a0, $zero, 5   # set input argument to 5
+        sub $s2, $s0, $s1  # n-k
+
+        #confirmation for testing
+        li $v0, 4
+        la $a0, confirm
+        syscall
+
+        li $v0, 1
+        addi $a0, $s0, 0
+        syscall
+
+        li $v0, 4  # separator
+        la $a0, sep
+        syscall
+
+        li $v0, 1
+        addi $a0, $s1, 0
+        syscall
+
+        li $v0, 4  # separator
+        la $a0, sep 
+        syscall
+
+        li $v0, 1
+        addi $a0, $s2, 0
+        syscall
+        #----
+
+
+	#factorials
+        add $a0, $zero, $s0   # set input argument to first input
         jal pfctrl
+	addi $s3, $v0, 0 # factorial(n)
 
-	addi $s0, $v0, 0 #store output in $s0.
+        add $a0, $zero, $s1  #set input argument to second input
+        jal pfctrl
+        addi $s4, $v0, 0 # factorial(k)
+    
+        add $a0, $zero, $s2  #set input argument to n-k
+        jal pfctrl
+        addi $s5, $v0, 0 # factorial(n-k)
+
+
+        mul $s6, $s4, $s5  # factorial(k) * factorial(n-k)
+        div $s6, $s3, $s6  # factorial(n) / prev
+
 
         li $v0, 4  # print string
         la $a0, out_str
         syscall
 
         li $v0, 1  #print int
-        addi $a0, $s0, 0  #move result into sys argument
+        addi $a0, $s6, 0  #move result into sys argument
         syscall
 
         addi $v0, $zero, 10  #finished, exit
@@ -65,5 +111,10 @@ in_n:
 in_k:
     .asciiz " Input the number of items chosen: "
 
+confirm:
+    .asciiz " N, K, (N-k) are: "
+
+sep:
+    .asciiz ", "
 # P Snyder 22 April 2015
 ######### End of the subroutine
