53f4f19b34d12db73051fb251541fedc475fd153
Justin Ely <justincely@gmail.com>
2015-10-25 17:08:50 -0400

    progress with new methods, needs refactor

--- JHU/615.202.81.FA15_data_structures/module_08/src/Matrix.java
+++ JHU/615.202.81.FA15_data_structures/module_08/src/Matrix.java
@@ -1,9 +1,15 @@
 public class Matrix{
   private int[][] data;
 
+  public Matrix(int order, int[] values){
+    data = new int[order][order];
+    loadData(values);
+    System.out.println("Matrix of order " + data.length + " initialized.");
+  }
+
   public Matrix(int order){
     data = new int[order][order];
-    System.out.println("Matrix of order " + data.length + " initialized.");
+    System.out.println("Matrix of order " + data.length + " initialized to 0.");
   }
 
   public Matrix(){
@@ -28,11 +34,20 @@ public class Matrix{
     return data;
   }
 
-  public int[][] Minor(int[][] a, int i, int j){
+  public void loadData(int[] values){
+    // needs error checking
+    int size = data.length;
+    for (int i=0; i<values.length; i++){
+      data[i%size][i/size] = values[i];
+    }
+  }
+
+  public Matrix Minor(int[][] a, int i, int j){
     int insize = a.length;
     int outsize = insize--;
     int[][] out = new int[outsize][outsize];
 
+    //FIX
     for (int x=0; x<insize; x++){
       for (int y=0; y<insize; y++){
         if ((x == i) || (y == j)){
@@ -42,7 +57,14 @@ public class Matrix{
         }
       }
     }
-    return out;
+
+    int[] out_1d = new int[outsize * outsize];
+    for (int x=0; x<insize; x++){
+      for (int y=0; y<insize; y++){
+        out_1d[y*insize + x] = out[x][y];
+      }
+    }
+    return new Matrix(outsize, out_1d);
   }
 
 }
--- JHU/615.202.81.FA15_data_structures/module_08/src/TestMatrix.java
+++ JHU/615.202.81.FA15_data_structures/module_08/src/TestMatrix.java
@@ -14,8 +14,13 @@ public class TestMatrix {
    @Test
    public void testMatrixMinor() {
      System.out.println("Testing the minor method");
-     Matrix myMatrix = new Matrix(6);
-     //int[][] m = myMatrix.minor(myMatrix, 0, 0);
+     Matrix myMatrix = new Matrix(3);
+     int[] data = {1, 1, 1, 2, 2, 2, 3, 3, 3};
+     myMatrix.loadData(data);
+     myMatrix.Print();
+
+     Matrix smallMatrix = myMatrix.Minor(myMatrix.getData(), 0, 0);
+     smallMatrix.Print();
    }
 
    @Test
@@ -25,5 +30,13 @@ public class TestMatrix {
      myMatrix.Print();
    }
 
+   @Test
+   public void testLoad() {
+     System.out.println("Testing the print method");
+     Matrix myMatrix = new Matrix(3);
+     int[] data = {1, 1, 1, 2, 2, 2, 3, 3, 3};
+     myMatrix.loadData(data);
+     myMatrix.Print();
+   }
 
 }
