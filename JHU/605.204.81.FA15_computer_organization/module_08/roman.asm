70365ca657123a7bcff09c954827b9a5e1c39f9f
Justin Ely <justincely@gmail.com>
2015-10-26 23:21:52 -0400

    progress

--- JHU/605.204.81.FA15_computer_organization/module_08/roman.asm
+++ JHU/605.204.81.FA15_computer_organization/module_08/roman.asm
@@ -14,21 +14,42 @@
         li $v0, 8 # read string
         la $a0, buffer
         li $a1, 20
-        move $t0, $a0
+        move $s0, $a0
         syscall
-
-        # $a0 = buffer, $a1 = length
 
         #-- verify input
         li $v0, 4  # print string
         la $a0, verify
         syscall
         la $a0, buffer
-        move $a0, $t0
+        move $a0, $s0
         li $v0, 4
         syscall
 
+        #-- convert
+        addi $s1, $zero, 0 #counter
+loop:
+        lb $s2, 0($s0)  # character
 
+        #-- print character
+        li $v0, 11 #print character
+        addi $a0, $s2, 0
+        syscall
+        #--
+
+        #jal arabic
+        #li $v0, 1
+        #addi $a0, $s0, 0
+        #syscall
+
+        #-- check for null and exit
+        beq $s2, $zero, exit
+        addi $s1, $s1, 1 # Count = Count + 1
+        addi $s0, $s0, 1
+        j loop
+
+
+exit:
         #-- Print final and exit
         li $v0, 4  # print string
         la $a0, final
@@ -38,9 +59,40 @@
         syscall
 
 
+#--- find value of character
+arabic:
+        sw $ra, 4($sp)
+        sw $a0, 0($sp)
+        addi $sp, $sp, -8
+
+        la $t0, numerals
+        la $t1, values
+
+next:
+        lb $t2, 0($t0)  #grab next roman numeral
+
+        #-- print character
+        li $v0, 11 #print character
+        addi $a0, $t2, 0
+        syscall
+        #--
+
+        beq $t2, $a0, return
+        addi $t0, $t0, 1
+        addi $t1, $t1, 1
+        j next
+
+return:
+        lb $v0, 0($t1)  #grab numeric value
+        jr $ra   # return
+
+
 .data
 buffer:
     .space 20
+
+numerals: .asciiz "IVXLCDMivxlcdm"
+values: .byte 1, 5, 10, 50, 100, 500, 1000, 1, 5, 10, 50, 100, 500, 1000
 
 greeting:
     .asciiz "Roman to Arabia converter:\nInput values should be in UPPER case and no more than 20 characters:\n"
