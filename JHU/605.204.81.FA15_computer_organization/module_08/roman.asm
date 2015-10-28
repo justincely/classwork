fcce3635df3438222035db76abc4703cd633c364
Justin Ely <justincely@gmail.com>
2015-10-27 20:36:32 -0400

    cleaned up and added comments

--- JHU/605.204.81.FA15_computer_organization/module_08/roman.asm
+++ JHU/605.204.81.FA15_computer_organization/module_08/roman.asm
@@ -2,22 +2,25 @@
 
 .text
  main:
-        #-- Greeting and instructions
-        li $v0, 4  # print string
+        #-----------------------------
+        #-- Greeting, instructions
+        #-- read input
+        #-----------------------------
+        li $v0, 4
         la $a0, greeting
         syscall
         la $a0, ask
         syscall
 
-        #-- read Input
-        li $v0, 8 # read string
+        #-- read Input string
+        li $v0, 8
         la $a0, buffer
         li $a1, 20
         move $s0, $a0
         syscall
 
-        #-- verify input
-        li $v0, 4  # print string
+        #-- print out input as verification
+        li $v0, 4
         la $a0, verify
         syscall
         la $a0, buffer
@@ -25,99 +28,63 @@
         li $v0, 4
         syscall
 
-        ####
-        # as of here - only $s0 should be saved
-        ####
 
-        addi $t1, $s0, 0 #tmp store location
+        #--------------------------
+        #-- Strip newline character
+        #-- from input
+        #--------------------------
+        addi $t1, $s0, 0            # tmp store location
 remove:
-        lb $a3, 0($s0)    # Load character at index
-        addi $s0, $s0, 1      # Increment index
-        bnez $a3, remove     # Loop until the end of string is reached
-        beq $a1, $s0, skip    # Do not remove \n when string = maxlength
-        addi $s0, $s0, -2     # If above not true, Backtrack index to '\n'
-        sb $0, 0($s0)    # Add the terminating character in its place
+        lb $a3, 0($s0)              # Load character at index
+        addi $s0, $s0, 1            # Increment index
+        bnez $a3, remove            # Loop until the end of string is reached
+        beq $a1, $s0, skip          # Do not remove \n when string = maxlength
+        addi $s0, $s0, -2           # If above not true, Backtrack index to '\n'
+        sb $0, 0($s0)               # Add the terminating character in its place
 skip:
-
-        addi $s0, $t1, 0 #restore location
-
-        ####
-        # as of here - only $s0 should be saved
-        ####
+        addi $s0, $t1, 0            #restore location
 
 
-
-        ####
-        # $s0, $s2, $s3, $s4, $s5
-
-        # s4 - value
-        # s5 - next value
-        ####
-        #-- convert
-        addi $s3, $zero, 0 #output value init to 0
+        #--------------------------
+        #-- Begin conversion
+        #-------------------------
+        addi $s3, $zero, 0          # output value init to 0
 loop:
-        lb $s2, 0($s0)  # character
+        lb $s2, 0($s0)              # character
 
-        #-- check for null and exit
-        beq $s2, $zero, exit
+        beq $s2, $zero, exit        # check for null and exit
 
-        #-- calculate first numeric
-        addi $a0, $s2, 0
-        jal arabic
-        move $s4, $v0  #grab value of numeral.
-
-        #-- print character
-        #li $v0, 11
-        #addi $a0, $s2, 0
-        #syscall
-
-        #li $v0, 4
-        #la $a0, sep
-        #syscall
-
-        #li $v0, 1
-        #addi $a0, $s4, 0
-        #syscall
-        #---
-
+        addi $a0, $s2, 0            # load character as argument
+        jal arabic                  # calculate first numeric
+        move $s4, $v0               # grab returned value of numeral.
 
         #-- Grab next if possible
-        addi $t1, $s0, 1
-        lb $s5, 0($t1)
-        beq $s5, $zero, justadd  #if next is the end go to the add
-        move $a0, $s5
-        jal arabic
-        move $s5, $v0 #grab returned value
+        addi $t1, $s0, 1            # offset cursor to next value
+        lb $s5, 0($t1)              # load the character
+        beq $s5, $zero, justadd     # if next is the end go to the add
+        move $a0, $s5               # load character as arguement
+        jal arabic                  # comput second numeric
+        move $s5, $v0               # grab returned value of numeral
 
-        #-- print character
-        #li $v0, 11
-        #addi $a0, $s2, 0
-        #syscall
+        #---------------------------------
+        #-- conditional block
+        #-- to deal with subtraction rules
+        #----------------------------------
 
-        #li $v0, 4
-        #la $a0, sep
-        #syscall
-
-        #li $v0, 1
-        #addi $a0, $s5, 0
-        #syscall
-        #---
-
-        #-- conditionals
         #-- if value = 1, next = 5
-        addi $t7, $zero, 1
-        seq $t1, $s4, $t7
+        addi $t7, $zero, 1          # t7 = 1
+        seq $t1, $s4, $t7           # if value == 1, t1 = 1
 
-        addi $t7, $zero, 5
-        seq $t2, $s5, $t7
+        addi $t7, $zero, 5          # t7 = 5
+        seq $t2, $s5, $t7           # if next == 5, t2 = 1
 
-        and $t3, $t1, $t2
-        addi $t4, $zero, 1
-        beq $t3, $t4, subadd
+        and $t3, $t1, $t2           # if (value == 1) and (next == 5): t3 = 1
+        addi $t4, $zero, 1          # t4 = 1
+        beq $t3, $t4, subadd        # if conditional is true, goto subtraction
 
         #-- if value = 1, next = 10
-        addi $t7, $zero, 1
-        seq $t1, $s4, $t7
+        addi $t7, $zero, 1          # ditto above, for each set of possible
+        seq $t1, $s4, $t7           # triggering values for subtraction
 
         addi $t7, $zero, 10
         seq $t2, $s5, $t7
@@ -170,48 +137,51 @@ loop:
         addi $t4, $zero, 1
         beq $t3, $t4, subadd
 
-        #else
-        j justadd
+        j justadd                   # else, add
 
 justadd:
+        #--------------------------
+        # Add value to running sum
+        #--------------------------
         add $s3, $s3, $s4
         addi $s0, $s0, 1
         j loop
 subadd:
+        #------------------------------
+        # subtract current from next
+        # before adding to running sum
+        #------------------------------
         sub $t1, $s5, $s4
         add $s3, $s3, $t1
         addi $s0, $s0, 2
         j loop
 
-
-
 exit:
-        #print results
-        li $v0, 4
+        #------------------------
+        #-- Print values and exit
+        #------------------------
+        li $v0, 4                   # print result string
         la $a0, decimal
         syscall
 
-        li $v0, 1
+        li $v0, 1                   # print result number
         addi $a0, $s3, 0
         syscall
 
-        #-- Print final and exit
-        li $v0, 4  # print string
+        li $v0, 4                   # print final message
         la $a0, final
         syscall
 
-        addi $v0, $zero, 10  #finished, exit
+        addi $v0, $zero, 10         # exit
         syscall
 
-
-
-
+#---------------------------
 #--- find value of character
+#---------------------------
 arabic:
         sw $ra, 4($sp)
         sw $a0, 0($sp)
         addi $sp, $sp, -8
-
 
         move $s7, $a0  #store argument
         la $t0, numerals
@@ -227,26 +197,13 @@ next:
         j next
 
 return:
-        #-- print character
-        #li $v0, 11
-        #addi $a0, $s7, 0
-        #syscall
-
-        #li $v0, 4
-        #la $a0, sep
-        #syscall
-
-        #li $v0, 1
-        #addi $a0, $t3, 0
-        #syscall
-        #---
         move $v0, $t3
         jr $ra   # return
 
 
-
-
-
+#--------------#
+#  Data below
+#--------------#
 .data
 buffer:
     .space 20
@@ -255,22 +212,16 @@ numerals: .asciiz "IVXLCDMivxlcdm"
 values: .word 1, 5, 10, 50, 100, 500, 1000, 1, 5, 10, 50, 100, 500, 1000
 
 greeting:
-    .asciiz "Roman to Arabia converter:\nInput values should be in UPPER case and no more than 20 characters:\n"
+    .asciiz "#-- Roman to Arabia converter --#\nInput values may be in either upper or lower case \nand should be no more than 20 characters long.\n"
 
 ask:
-    .asciiz "String to convert?\n"
+    .asciiz "What would you like to convert?\n"
 
 decimal:
-    .asciiz "\nThe decimal value is :"
+    .asciiz "\nThe decimal value is: "
 
 final:
-    .asciiz "\n--The converter has terminated--"
+    .asciiz "\n#-- The converter has terminated --#"
 
 verify:
     .asciiz "\nYou have input:"
-
-sep:
-    .asciiz " "
-
-newline:
-    .asciiz "\n"
