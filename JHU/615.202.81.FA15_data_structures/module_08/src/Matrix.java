659a0aff06d585b4415a9c19d3fd20d826f0ce24
Justin Ely <justincely@gmail.com>
2015-10-29 08:52:54 -0400

    added documentation to matrix code

--- JHU/615.202.81.FA15_data_structures/module_08/src/Matrix.java
+++ JHU/615.202.81.FA15_data_structures/module_08/src/Matrix.java
@@ -1,21 +1,54 @@
+/** A Matrix of <code>int</code> precision.
+  *
+  * <p>The matrix is implemented as an array
+  *     with an arbitrary maximum size.
+  * </p>
+  *
+  * @author Justin Ely
+  * @version 0.0.1
+  * @license BSD 3-clause
+  */
 public class Matrix{
+  // Holds the 2d matrix in a 2d array
   private int[][] data;
 
+  /**Construct matrix with order and data arguments
+    *
+    *
+    *@param int order - the order n of the nxn matrix
+    *@param int[] values - a 1D array of input values (flattened from 2d array)
+    */
   public Matrix(int order, int[] values){
     data = new int[order][order];
     loadData(values);
-    //System.out.println("Matrix of order " + data.length + " initialized.");
   }
 
+  /**Construct matrix with just order argument
+    *
+    *<p> Data will be initialized to 0 according to java
+    *    language specifications.
+    *</p>
+    *
+    *@param int order - the order n of the nxn matrix
+    */
   public Matrix(int order){
     data = new int[order][order];
-    //System.out.println("Matrix of order " + data.length + " initialized to 0.");
   }
 
+  /**Construct default matrix
+    *
+    *<p> Matrix of order 1 will be initialized to 0 according to java
+    *    language specifications.
+    *</p>
+    */
   public Matrix(){
     this(1);
   }
 
+  /**Calculate Determinate of a matrix
+    *
+    *@param Matrix mat - matrix object on which to calculate determinate.
+    */
   public static long Determinate(Matrix mat){
     int[][] subData = mat.getData();
     if (subData.length == 1){
@@ -31,6 +64,14 @@ public class Matrix{
     }
   }
 
+  /**Print matrix content to STDOUT
+    *
+    *<p> Convenience function to display matrix contents
+    *    To the screen.  Will iterate over array indeces and
+    *    display each row on a new line.  Columns will be separated
+    *    by spaces.  Termination will be in a newline character.
+    *</p>
+    */
   public void Print(){
     int size = data.length;
     for (int y=0; y<size; y++){
@@ -41,10 +82,24 @@ public class Matrix{
     }
   }
 
+  /** Fetch core 2d data array
+    *
+    *@return int[][] data - the core data of the matrix
+    */
   public int[][] getData(){
     return data;
   }
 
+
+  /** Load values into the matrix data structure
+    *
+    *<p> Load data values into the matrix.  Input data should be the
+    *    same size as the matrix, but in 1D form represented as concatenated
+    *    rows.
+    *</p>
+    *
+    *@param int[] values - the 1d representation of the 2d input array.
+    */
   public void loadData(int[] values){
     // needs error checking
     int size = data.length;
@@ -53,11 +108,16 @@ public class Matrix{
     }
   }
 
+  /** Pull out the Minor of the matrix
+    *
+    *@param int i - row of the Minor
+    *@param int j - column of the Minor
+    *@return Matrix - minor matrix of order n-1
+    */
   public Matrix Minor(int i, int j){
     int insize = data.length;
     int outsize = insize - 1;
     int[] out_data = new int[outsize*outsize];
-    //System.out.println("Finding the minor of matrix");
 
     for (int x=0; x<insize; x++){
       for (int y=0; y<insize; y++){
