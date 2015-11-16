2b0c8a7bd801a43d15e865fabbc9653b6d1a3981
Justin Ely <justincely@gmail.com>
2015-11-16 12:29:36 -0500

    cleared out old code from other module

---
+++ JHU/615.202.81.FA15_data_structures/module_11/src/Driver.java
@@ -0,0 +1,38 @@
+/**Driver
+  *
+  */
+
+import java.util.Scanner;
+import java.io.File;
+
+
+/**Parse matrices and calculate determinates.
+  */
+public class Driver{
+
+/**Main driver to open files
+  *
+  *@param args[]   Holds command line arguments: filename.
+  */
+ public static void main(String[]args){
+   int matrixSize = 0;
+   int[] inData = new int[1];
+
+   /* Loop over input files */
+   for (int i=0; i < args.length; i++) {
+     try{
+       Scanner scanner = new Scanner(new File(args[i]));
+       System.out.println("Reading file " + args[i]);
+
+       while(scanner.hasNext()){
+       }
+
+     } catch (java.io.FileNotFoundException e){
+       System.out.println("Cannot find file: " + args[i]);
+       return;
+     }
+
+   }
+   System.out.println("--Finished--");
+ }
+}
---
+++ JHU/615.202.81.FA15_data_structures/module_11/src/TestRunner.java
@@ -0,0 +1,19 @@
+/**TestRunner
+  *
+  * Run all unittests defined in TestMatrix
+  */
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+/**Run all tests in the TestMatrix class
+  */
+public class TestRunner {
+   public static void main(String[] args) {
+      Result result = JUnitCore.runClasses(TestXXXXXXXX.class);
+      for (Failure failure : result.getFailures()) {
+         System.out.println(failure.toString());
+      }
+      System.out.println(result.wasSuccessful());
+   }
+}
