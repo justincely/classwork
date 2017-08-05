48d1e6ac2def1bec0d3d52526390639c3df132cb
Justin Ely <justin.ely@deciphernow.com>
2017-08-05 14:18:50 -0400

    fix query to ALL from ANY

--- JHU/intro_db/hw4/query_db.sql
+++ JHU/intro_db/hw4/query_db.sql
@@ -1,3 +1,6 @@
 -- Retrieve the SSNs of employees who work on all projects that John Smith works on
-SELECT DISTINCT ssn FROM employee JOIN works_on ON employee.ssn=works_on.Essn WHERE pno IN
-  (SELECT DISTINCT pno FROM employee JOIN works_on ON employee.ssn=works_on.Essn WHERE Fname='John' and Lname='Smith');
+SELECT DISTINCT ssn FROM employee JOIN works_on ON employee.ssn=works_on.Essn
+  WHERE pno IN
+    (SELECT DISTINCT pno FROM employee JOIN works_on ON employee.ssn=works_on.Essn WHERE Fname='John' and Lname='Smith')
+  GROUP BY ssn
+  HAVING COUNT(*) = 2;
