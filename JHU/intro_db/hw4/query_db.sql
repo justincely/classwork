f7521a0e4e785e1db72d535a49691d6e3737f3b5
Justin Ely <justin.ely@deciphernow.com>
2017-08-04 23:26:56 -0400

    create and populate database

---
+++ JHU/intro_db/hw4/query_db.sql
@@ -0,0 +1,3 @@
+-- Retrieve the SSNs of employees who work on all projects that John Smith works on
+SELECT DISTINCT ssn FROM employee JOIN works_on ON employee.ssn=works_on.Essn WHERE pno IN
+  (SELECT DISTINCT pno FROM employee JOIN works_on ON employee.ssn=works_on.Essn WHERE Fname='John' and Lname='Smith');
