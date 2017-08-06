53a402b67dd0690eead4b9f4a8692bd30e4884bf
Justin Ely <justin.ely@deciphernow.com>
2017-08-05 22:35:50 -0400

    prettier formatting

--- JHU/intro_db/hw4/query_db.sql
+++ JHU/intro_db/hw4/query_db.sql
@@ -1,6 +1,13 @@
 -- Retrieve the SSNs of employees who work on all projects that John Smith works on
-SELECT DISTINCT ssn FROM employee JOIN works_on ON employee.ssn=works_on.Essn
+SELECT DISTINCT ssn
+  FROM employee
+    JOIN works_on ON employee.ssn=works_on.Essn
   WHERE pno IN
-    (SELECT DISTINCT pno FROM employee JOIN works_on ON employee.ssn=works_on.Essn WHERE Fname='John' and Lname='Smith')
+    (
+      SELECT DISTINCT pno
+      FROM employee
+        JOIN works_on ON employee.ssn=works_on.Essn
+      WHERE Fname='John' and Lname='Smith'
+    )
   GROUP BY ssn
   HAVING COUNT(*) = 2;
