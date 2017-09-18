97a7b4d111b8fa5f34587ebf2884b1426a215dd7
Justin Ely <justincely@gmail.com>
2017-09-17 21:32:52 -0400

    postings list mostly cool

--- JHU/information_retrieval/prog_1/corpus_stats.py
+++ JHU/information_retrieval/prog_1/corpus_stats.py
@@ -1,4 +1,5 @@
-from collections import Counter
+from collections import Counter, namedtuple
+import struct
 import sys
 
 from bs4 import BeautifulSoup
@@ -28,10 +29,61 @@ def clean(text):
     for character in ['.', ':', ';', '?', '!', '-']:
         text = text.replace(character, '')
 
+    for character in [',']:
+        text = text.replace(character, ' ')
+
     text = text.lower().split()
 
     return text
 
+def create_posting_list(filename):
+    Posting = namedtuple("Posting", ["doc_id", "count"])
+    postings_list = dict()
+
+    body = BeautifulSoup(open(fname), "html.parser")
+
+    for i, tag in enumerate(body.find_all('p')[:2]):
+        text = clean(tag.text)
+        vocabulary = Counter(text)
+
+        for word, count in vocabulary.items():
+            if not word in postings_list:
+                postings_list[word] = []
+            postings_list[word].append(Posting(int(tag.attrs['id']), count))
+
+    return postings_list
+
+
+def write_posting_list(posting_list, outname='posting_list.dat'):
+    corpus_dict = dict()
+    Term = namedtuple("term", ["frequency", "offset"])
+
+    last_freq = 0
+    with open(outname, 'wb') as outfile:
+        for word in sorted(posting_list):
+
+            word_doc_freq = len(posting_list[word])
+            corpus_dict[word] = Term(word_doc_freq, last_freq * 4 * 2)
+            last_freq += word_doc_freq
+
+            for post in sorted(posting_list[word], key=lambda item: item.doc_id):
+                outfile.write(post.doc_id.to_bytes(4, byteorder='big'))
+                outfile.write(post.count.to_bytes(4, byteorder='big'))
+
+    for word in sorted(corpus_dict):
+        print(word, corpus_dict[word])
+
+    return corpus_dict
+
+def parse_posting_list(fname, dictionary):
+    with open(fname, 'rb') as infile:
+        for word, values in dictionary.items():
+            
+            infile.seek(values.offset)
+            for i in range(values.frequency):
+                doc_id = struct.unpack('>I', infile.read(4))[0]
+                doc_freq = struct.unpack('>I', infile.read(4))[0]
+                print("{} {} {}".format(word, doc_id, doc_freq))
 
 def analyze_file(filename):
     """Perform aggregate analysis for a filename
@@ -53,15 +105,21 @@ def analyze_file(filename):
 
     collection_freq = Counter()
     document_freq = Counter()
+    Posting = namedtuple("Posting", ["doc_id", "count"])
+    postings_list = dict()
 
     body = BeautifulSoup(open(fname), "html.parser")
 
     for i, tag in enumerate(body.find_all('p')):
         text = clean(tag.text)
         vocabulary = Counter(text)
-
         collection_freq.update(vocabulary)
         document_freq.update(Counter(vocabulary.keys()))
+
+        for word, count in vocabulary.items():
+            if not word in postings_list:
+                postings_list[word] = []
+            postings_list[word].append(Posting(tag.attrs['id'], count))
 
     return i+1, collection_freq, document_freq
 
@@ -71,26 +129,7 @@ if __name__ == "__main__":
     fname = sys.argv[1]
     print("Running on {}".format(fname))
 
-    doc_count, collection_freq, document_freq = analyze_file(fname)
-
-    print("N Documents: {}".format(doc_count))
-    print("N Unique Words: {}".format(len(collection_freq.keys())))
-    print("N Words: {}".format(sum(collection_freq.values())))
-    print()
-
-    print("The 30 Most Common words:")
-    print("{:10} {:6} {:6}".format("word", "times", "docs"))
-    print("-------------------------")
-    for key, val in collection_freq.most_common()[:30]:
-        print("{:10} {:6} {:6}".format(key, val, document_freq[key]))
-    print()
-
-    print("Nth most common")
-    print("---------------")
-    print("100th:  {}".format(collection_freq.most_common()[100]))
-    print("500th:  {}".format(collection_freq.most_common()[500]))
-    print("1000th: {}".format(collection_freq.most_common()[1000]))
-    print()
-
-    only_1 = sum([val for val in document_freq.values() if val == 1])
-    print("Number of words that occur in exactly 1 document: {}".format(only_1))
+    posting_list = create_posting_list(fname)
+    print(posting_list)
+    corpus_dict = write_posting_list(posting_list)
+    parse_posting_list('posting_list.dat', corpus_dict)
